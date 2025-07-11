6aea70dcf37e92142674e1578ba939f0c058b271
itsmearnab12 <arnabchoudhury2005@gmail.com>
2025-07-11 19:04:56 +0530

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Now I Will start writing javascript")
+async function getsongs(){
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let element = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            sessionStorage.push(element.href)
+        }
+    }
+    return songs
+}
+let songs = getsongs()
+console.log(songs)
